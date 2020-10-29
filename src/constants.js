b7c42f17f339e85712071b86aec226bd1a34b081
Eivenlour David <eivenlou@ualberta.ca>
2020-10-29 11:58:27 -0600

    feat: add generate report implementation (#9)
    * feat: add generate report implementation
    
    * fix: indentation
    
    * chore: remove comments
    
    * fix: stop tracking .env file

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const categories = {
+  ACCESSIBILITY: 'accessibility',
+  BEST_PRACTICES: 'best-practices',
+  PERFORMANCE: 'performance',
+  PWA: 'pwa',
+  SEO: 'seo'
+};
+
+const deviceForms = {
+  DESKTOP: 'desktop',
+  MOBILE: 'mobile',
+  NONE: 'none'
+}
+
+module.exports = { 
+  categories,
+  deviceForms
+}
